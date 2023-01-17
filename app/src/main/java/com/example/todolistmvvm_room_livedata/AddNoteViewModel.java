3cea290399d9f73ba4c2adf3d72f82e7b58e5b9b
And <dron1515@gmail.com>
2023-01-17 15:41:10 +0300

    RxJava CompositeDisposable and Disposable

--- app/src/main/java/com/example/todolistmvvm_room_livedata/AddNoteViewModel.java
+++ app/src/main/java/com/example/todolistmvvm_room_livedata/AddNoteViewModel.java
@@ -1,16 +1,26 @@
 package com.example.todolistmvvm_room_livedata;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class AddNoteViewModel extends AndroidViewModel {
 
     private NotesDao notesDao;
     private MutableLiveData<Boolean> shouldCloseScreen = new MutableLiveData<>();
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public AddNoteViewModel(@NonNull Application application) {
         super(application);
@@ -23,13 +33,27 @@ public class AddNoteViewModel extends AndroidViewModel {
     }
 
     public void saveNote(Note note){
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                notesDao.add(note);
-                shouldCloseScreen.postValue(true);
-            }
-        });
-        thread.start();
+        Disposable disposable = notesDao.add(note)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        shouldCloseScreen.setValue(true);
+
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Throwable {
+                        Log.d("AddNoteViewModel", "Ошибка saveNote");
+                    }
+                });
+            compositeDisposable.add(disposable);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }
--- app/src/main/java/com/example/todolistmvvm_room_livedata/MainViewModel.java
+++ app/src/main/java/com/example/todolistmvvm_room_livedata/MainViewModel.java
@@ -1,15 +1,24 @@
 package com.example.todolistmvvm_room_livedata;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class MainViewModel extends AddNoteViewModel{
 
     private NoteDB noteDB;
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public MainViewModel(@NonNull Application application) {
         super(application);
@@ -21,12 +30,25 @@ public class MainViewModel extends AddNoteViewModel{
     }
 
     public void remove(Note note){
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                noteDB.notesDao().remove(note.getId());
-            }
-        });
-        thread.start();
+        Disposable disposable = noteDB.notesDao().remove(note.getId())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Throwable {
+                        Log.d("MainViewModel", "Ошибка remove");
+                    }
+                });
+        compositeDisposable.add(disposable);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }
